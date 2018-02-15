d0744617788181e78f6b4bf86c8f87a28d5f3933
Robin Hes <robinhes@outlook.com>
2018-02-15 12:57:01 +0100

    Added pairwise distance matrix function.

--- cluster/util/_dissim.pxd
+++ cluster/util/_dissim.pxd
@@ -1,2 +1,3 @@
 cdef double _euclidean_dissim(double[:], double[:])
 cdef int _matching_dissim(long[:], long[:])
+cpdef pairwise_dissim(double[:, :], long[:, :], double[:, :], long[:, :], double)
--- cluster/util/_dissim.pyx
+++ cluster/util/_dissim.pyx
@@ -3,7 +3,9 @@
 # cython: boundscheck=False
 # cython: wraparound=False
 
+import numpy as np
 cimport cython
+cimport numpy as np
 
 @cython.profile(False)
 cdef double _euclidean_dissim(double[:] centroid, double[:] x):
@@ -27,5 +29,19 @@ cdef int _matching_dissim(long[:] centroid, long[:] x):
     for i in range(x.shape[0]):
         tmp = (centroid[i] != x[i])
         result += tmp
+
+    return result
+
+cpdef pairwise_dissim(double[:, :] x_num, long[:, :] x_cat, double[:, :] y_num, long[:, :] y_cat, double gamma):
+    result = np.zeros((x_num.shape[0], y_num.shape[0]), dtype=np.float)
+
+    cdef int i, j
+    cdef double a
+    cdef long b
+    for i in range(x_num.shape[0]):
+        for j in range(y_num.shape[0]):
+            a = _euclidean_dissim(y_num[j], x_num[i])
+            b = _matching_dissim(y_cat[j], x_cat[i])
+            result[i, j] = a + gamma * b
 
     return result
